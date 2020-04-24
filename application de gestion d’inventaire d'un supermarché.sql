4d8cd65100a790a15fad12f2658280af99b95cb4
ayyoubhalbaoui1 <ayyoubhalbaoui1@gmail.com>
2020-04-24 16:20:12 +0000

    modification sur les files

--- application de gestion d’inventaire d'un supermarché.sql
+++ application de gestion d’inventaire d'un supermarché.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 24, 2020 at 02:26 PM
+-- Generation Time: Apr 24, 2020 at 04:14 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -35,22 +35,7 @@ CREATE TABLE IF NOT EXISTS `inventery` (
   `DateRegistered` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `order1`
---
-
-DROP TABLE IF EXISTS `order1`;
-CREATE TABLE IF NOT EXISTS `order1` (
-  `SrockID` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `Password` char(255) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  `deliverdate` char(1) DEFAULT NULL,
-  `date` char(1) DEFAULT NULL,
-  `DateRegistered` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -90,24 +75,41 @@ CREATE TABLE IF NOT EXISTS `product` (
   `ID` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `date` date DEFAULT NULL,
-  `price` float DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
   `expiredDate` date DEFAULT NULL,
+  `Rayon_id` int(11) NOT NULL
   `DateRegistered` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `regusers`
+-- Table structure for table `provider`
 --
 
-DROP TABLE IF EXISTS `regusers`;
-CREATE TABLE IF NOT EXISTS `regusers` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `Password` char(255) NOT NULL,
-  `DateRegistered` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `provider`;
+CREATE TABLE IF NOT EXISTS `provider` (
+  `id` int(11) NOT NULL,
+  `Firts _name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Company` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Tel` int(10) NOT NULL,
+  `Email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rayons`
+--
+
+DROP TABLE IF EXISTS `rayons`;
+CREATE TABLE IF NOT EXISTS `rayons` (
+  `Rayon_id` int(11) NOT NULL,
+  `Rayon_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
 
 -- --------------------------------------------------------
 
